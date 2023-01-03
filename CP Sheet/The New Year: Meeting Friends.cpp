d1038344e8326a5bdf3f80df24840667016a1a9a
Vatsal Bhatt <114211721+VatsBhatt11@users.noreply.github.com>
2023-01-04 01:27:36 +0530

    Create The New Year: Meeting Friends.cpp

---
+++ CP Sheet/The New Year: Meeting Friends.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int x;
+    vector<int>v;
+    for(int i=0;i<3;i++)
+    {
+        cin >> x;
+        v.push_back(x);
+    }
+    sort(v.begin(),v.end());
+    cout << (v[1]-v[0])+(v[2]-v[1]) << endl;
+    return 0;
+}
