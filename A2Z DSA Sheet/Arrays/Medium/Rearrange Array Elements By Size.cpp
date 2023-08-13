230768200f21f8acfcbdb163cb3f7579044055e0
VatsBhatt11 <vatsalmbhatt11@gmail.com>
2023-08-13 23:09:40 +0530

    Commit

---
+++ A2Z DSA Sheet/Arrays/Medium/Rearrange Array Elements By Size.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        vector<int>ans(nums.size());
+        int posi=0,negi=1;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]>0)
+            {
+                ans[posi]=nums[i];
+                posi+=2;
+            }
+            else
+            {
+                ans[negi]=nums[i];
+                negi+=2;
+            }
+        }
+        return ans;
+    }
+};
