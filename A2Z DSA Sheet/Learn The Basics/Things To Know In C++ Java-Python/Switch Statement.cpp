56d6dfb8b60dd830de973c6ed8ad409ed94c87f8
VatsBhatt11 <vatsalmbhatt11@gmail.com>
2023-08-18 11:08:46 +0530

    Commit

---
+++ A2Z DSA Sheet/Learn The Basics/Things To Know In C++ Java-Python/Switch Statement.cpp
@@ -0,0 +1,31 @@
+double areaSwitchCase(int ch, vector<double> a) 
+
+{
+
+    // Write your code here
+
+    switch(ch)
+
+    {
+
+        case 1:
+
+        {
+
+            double area = M_PI*a[0]*a[0]  ;
+
+            return area ;
+
+        }
+
+        case 2:
+
+        {
+
+            return (a[0]*a[1]);
+
+        }
+
+    }
+
+}
