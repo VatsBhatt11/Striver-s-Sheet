fdbade07c1a05964618ab2aa66812edea1261745
VatsBhatt11 <vatsalmbhatt11@gmail.com>
2023-08-27 09:05:41 +0530

    Commit

---
+++ A2Z DSA Sheet/Binary Search/BS On 1D Arrays/Implement Lower Bound.cpp
@@ -0,0 +1,20 @@
+int LB(vector<int>& nums, int target)
+    {
+        int l,u,a=-1,m;
+        l=0;
+        u=nums.size()-1;
+        while(l<=u)
+        {
+            m=(l+u)/2;
+            if(nums[m]==target)
+            {
+                a=m;
+                u=m-1;
+            }
+            else if(target<nums[m])
+                u=m-1;
+            else
+                l=m+1;
+        }
+        return a;
+    }
