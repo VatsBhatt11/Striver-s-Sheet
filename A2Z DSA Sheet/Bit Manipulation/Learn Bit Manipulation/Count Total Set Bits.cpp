cfdb13489f73a589bada0cdfaaedcfcf960ecc39
VatsBhatt11 <vatsalmbhatt11@gmail.com>
2023-09-04 09:59:19 +0530

    Commit

---
+++ A2Z DSA Sheet/Bit Manipulation/Learn Bit Manipulation/Count Total Set Bits.cpp
@@ -0,0 +1,7 @@
+int countSetBits(int N)
+{
+    long long int res = 0;
+    for(long long int i = 1; i < 32; i++)
+        res = res + ((N + 1)/(1 << i))*(1 << (i - 1)) + max(0,  (  ((N + 1)%(1 << i)) - (1 << (i - 1))    )  );
+    return res;
+}
