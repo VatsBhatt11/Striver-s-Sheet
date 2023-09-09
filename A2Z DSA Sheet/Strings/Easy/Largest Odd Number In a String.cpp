c7653ef3ef9a4f711f3bcd9b83bac1fc9fd326ff
VatsBhatt11 <vatsalmbhatt11@gmail.com>
2023-09-09 10:44:47 +0530

    Commit

---
+++ A2Z DSA Sheet/Strings/Easy/Largest Odd Number In a String.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string largestOddNumber(string num) {
+        int i;
+        for(i=num.size()-1;i>=0;i--)
+        {
+            if((num[i]-'0')&1)
+                break;
+        }
+        return num.substr(0,i+1);
+    }
+};
